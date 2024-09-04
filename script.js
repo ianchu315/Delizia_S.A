fca6600b0c17da021ede292f655b78aa38aa5995
ianchu315 <ianchuquintana2020@hotmail.com>
2024-09-03 23:15:38 -0300

    Inicio del proyecto
    login y parte de la pagina principal ya terminada

---
+++ script.js
@@ -0,0 +1,9 @@
+// // Añadir animación a las imágenes del carrusel cuando se deslizan
+// document.addEventListener('DOMContentLoaded', function () {
+//     $('#carouselExampleControls').on('slid.bs.carousel', function () {
+//         const items = document.querySelectorAll('.carousel-item');
+//         items.forEach(item => item.classList.remove('slide-in'));
+//         const activeItem = document.querySelector('.carousel-item.active');
+//         activeItem.classList.add('slide-in');
+//     });
+// });
